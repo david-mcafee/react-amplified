6e5045fe26a17fdfc1d6e78ee739c17bfa60a2d6
David McAfee <mcafd@amazon.com>
2021-12-15 10:12:03 -0800

    init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "reactamplifiedd7842634": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "api6a984c52": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import { Authenticator } from "@aws-amplify/ui-react";
+import { API } from "@aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+
+export default function App() {
+  function getData() {
+    const apiName = "MyApiName";
+    const path = "/path";
+    const myInit = {
+      // OPTIONAL
+      headers: {}, // OPTIONAL
+    };
+
+    return API.get(apiName, path, myInit);
+  }
+
+  (async function () {
+    const response = await getData();
+    console.log(response);
+  })();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
